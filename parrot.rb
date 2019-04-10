55f936b51b485be88d1e4bbf7f8f66172b1ce246
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-10 21:37:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(sound = "Squawk")
-  puts "Hello I'm a parrot #{sound}."
+  puts "I'm a pretty bird #{sound}."
 end
 parrot
